815024be217d4966abaa14e75e2ae80a0dddb7b3
itscactus <itscactusdev@gmail.com>
2023-04-30 14:19:15 +0200

    d

---
+++ app/rendered/projeler/page.jsx
@@ -0,0 +1,12 @@
+export default function AnaSayfa() {
+  return (
+    <>
+      <div className={`flex flex-col gap-2 text-md md:text-xl`}>
+        Destek Elçisi
+        <div className={`text-sm md:text-md text-bold`}>
+          Yaklaşık 3 senedir kodladığım Dashboardlı Discord Destek Botu<a href={'https://destekelcisi.com'}>Destek Elçisi</a>
+        </div>
+      </div>
+    </>
+  );
+}
