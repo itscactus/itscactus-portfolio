71968fe992add8bc5e80039b420b8906e40f367c
itscactus <itscactusdev@gmail.com>
2023-04-30 14:14:40 +0200

    d

---
+++ app/projeler/page.jsx
@@ -0,0 +1,81 @@
+import Image from 'next/image'
+export default function Hakkimda() {
+  return (
+    <>
+      <div className='flex flex-col'>
+        <div className="text-red-400">export default function <span className='text-purple-500'>AnaSayfa()</span> <span className='text-blue-500'>&#123;</span></div>
+        
+        <div className='ml-4 text-red-400'>return <span className='text-orange-500'>(</span></div>
+        <div className='ml-6 text-green-500'><span className='text-white'>{'<'}</span>body{' '}
+        <span className='text-purple-500'> 
+              className<span className='text-red-500'>=</span>
+              <span className='text-blue-500'>{'{'}</span>
+              <span className='text-blue-400'>{'`bg-zinc-800 font-mono`'}</span>
+              <span className='text-blue-500'>{'}'}</span>
+            </span>
+        <span className='text-white'>{'>'}</span></div>
+        {/* Hangi dilleri biliyorum */}
+        <div className='ml-8 text-green-500'>
+          <span className='text-white'>{'<'}</span>div{' '}
+            <span className='text-purple-500'> 
+              className<span className='text-red-500'>=</span>
+              <span className='text-blue-500'>{'{'}</span>
+              <span className='text-blue-400'>{'`flex flex-col gap-2 text-md md:text-xl`'}</span>
+              <span className='text-blue-500'>{'}'}</span>
+            </span>
+          <span className='text-white'>{'>'}</span>
+          <div className='ml-2 text-white'>Destek Elçisi</div>
+          <span className='ml-2 text-white'>{'<'}</span>div{' '}
+            <span className='text-purple-500'> 
+              className<span className='text-red-500'>=</span>
+              <span className='text-blue-500'>{'{'}</span>
+              <span className='text-blue-400'>{'`text-sm md:text-md text-bold`'}</span>
+              <span className='text-blue-500'>{'}'}</span>
+            </span>
+          <span className='text-white'>{'>'}</span>
+          <div className='text-white ml-4'>
+            Yaklaşık 3 senedir kodladığım Dashboardlı Discord Destek Botu
+            <span className='text-white'>{'<'}<span className='text-green-500'>a</span> <span className='text-blue-400'>href=<span className='text-blue-500'>{'{'}{"'"}https://destekelcisi.com{"'"}{'}'}</span></span> <span className='text-white'>{'>'}</span></span>
+          </div>
+          <div className='ml-4'>
+            <span className='text-blue-400'>{''}</span>
+          </div>
+          <span className='ml-2 text-white'>{'</'}</span>div<span className='text-white'>{'>'}</span>
+          <div></div>
+          <span className='text-white'>{'</'}</span>div<span className='text-white'>{'>'}</span>
+        </div> 
+         {/* Discord adresim */}
+        <div className='ml-8 text-green-500'>
+          <span className='text-white'>{'<'}</span>div{' '}
+            <span className='text-purple-500'> 
+              className<span className='text-red-500'>=</span>
+              <span className='text-blue-500'>{'{'}</span>
+              <span className='text-blue-400'>{'`flex flex-col gap-2 text-md md:text-xl text-bold`'}</span>
+              <span className='text-blue-500'>{'}'}</span>
+            </span>
+          <span className='text-white'>{'>'}</span>
+          <div className='ml-2 text-white'>Discord adresin nedir?</div>
+          <span className='ml-2 text-white'>{'<'}</span>div{' '}
+            <span className='text-purple-500'> 
+              className<span className='text-red-500'>=</span>
+              <span className='text-blue-500'>{'{'}</span>
+              <span className='text-blue-400'>{'`text-sm md:text-md`'}</span>
+              <span className='text-blue-500'>{'}'}</span>
+            </span>
+          <span className='text-white'>{'>'}</span>
+          <div className='text-white ml-4'>Discord Adresim: cactusdev#8952</div>
+          <div className='ml-4'>
+            <span className='text-blue-400'>{''}</span>
+          </div>
+          <span className='ml-2 text-white'>{'</'}</span>div<span className='text-white'>{'>'}</span>
+          <div></div>
+          <span className='text-white'>{'</'}</span>div<span className='text-white'>{'>'}</span>
+        </div> 
+       
+        <div className='ml-6 text-green-500'><span className='text-white'>{'</'}</span>body<span className='text-white'>{'>'}</span></div>
+        <div className='ml-4 text-orange-500'>)</div>
+        <div className='text-blue-500'>&#125;</div>
+      </div>
+    </>
+  )
+}
